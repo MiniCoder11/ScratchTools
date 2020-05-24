b02538749f0710d3665f9fba2b5c2509f5df57b3
MiniCoder11 <63173581+MiniCoder11@users.noreply.github.com>
2020-05-24 16:21:05 -0700

    Update ScratchTools.user.js

--- ScratchTools.user.js
+++ ScratchTools.user.js
@@ -15,37 +15,42 @@
     //Thanks to Scratch user, Boomer001 for finding and providing documentation for Scratch-VM.
     //Credit to Scratch user, _nix for documenting the below method to mount the Scratch-VM.
 
-    document.head.appendChild(document.createElement("script")).innerHTML = `
-const findVM = () => new Promise(resolve => {
-    const oldBind = Function.prototype.bind;
-    Function.prototype.bind = function(...args) {
-        if (args[0] && args[0].hasOwnProperty('editingTarget') && args[0].hasOwnProperty('runtime')) {
-            Function.prototype.bind = oldBind;
-            resolve(args[0]);
+    //document.head.appendChild(document.createElement("script")).innerHTML = `
+    const findVM = () => new Promise(resolve => {
+        const oldBind = Function.prototype.bind;
+        Function.prototype.bind = function(...args) {
+            if (args[0] && args[0].hasOwnProperty('editingTarget') && args[0].hasOwnProperty('runtime')) {
+                Function.prototype.bind = oldBind;
+                resolve(args[0]);
+            }
+            return oldBind.apply(this, args);
+        };
+    });
+
+    findVM().then(vm => {
+
+        var virtualMachine = vm;
+        console.log("ScratchTools: VM mounted successfully")
+
+        function setVar(sprite, name, value) {
+            virtualMachine.setVariableValue(virtualMachine.runtime.getSpriteTargetByName(sprite).id,virtualMachine.runtime.getSpriteTargetByName(sprite).lookupVariableByNameAndType(name).id,value);
         }
-        return oldBind.apply(this, args);
-    };
-});
-findVM().then(vm => {
-    virtualMachine = vm;
-    console.log("ScratchTools: VM mounted successfully")
 
-    function setVar(sprite, name, value) {
-        virtualMachine.setVariableValue(virtualMachine.runtime.getSpriteTargetByName(sprite).id,virtualMachine.runtime.getSpriteTargetByName(sprite).lookupVariableByNameAndType(name).id,value);
-    }
+        virtualMachine.setCompatibilityMode(false);
 
-    virtualMachine.setCompatibilityMode(false);
+        virtualMachine.setVariableValue(virtualMachine.runtime.getSpriteTargetByName("ScratchTools").id,virtualMachine.runtime.getSpriteTargetByName("ScratchTools").lookupVariableByNameAndType("OnMobile").id,7);
 
-//Mobile check (Thanks to Santhosh on stack overflow: https://stackoverflow.com/a/29509267)
+        //Mobile check (Thanks to Santhosh on stack overflow: https://stackoverflow.com/a/29509267)
 
-    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-        setVar("ScratchTools", "OnMobile", 1)
-    } else {
-        setVar("ScratchTools", "OnMobile", 0)
-    }
+        /*var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        if (isMobile) {
+            setVar("ScratchTools", "OnMobile", 34)
+        } else {
+            setVar("ScratchTools", "OnMobile", 34)
+        }
+        */
 
-});
-`;
+    });
+    //';
 
 })();
